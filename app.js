2154d8efe3acf73e6fbb97447fca391f5169bca2
Ondrej Cernobila <ondrej.cernobila@konicaminolta.cz>
2018-03-21 18:04:39 +0100

    added info button

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-app = angular.module("app", ["chart.js","daterangepicker","angularMoment"]);
-app.controller("LineCtrl", function ($scope, $http) {
-
-    
-
-
+app = angular.module("app", ["chart.js", "daterangepicker", "angularMoment", 'ngMaterial']);
+app.controller("ChartCtrl", function ($scope, $http) {
     $scope.onClick = function (points, evt) {
         console.log(points, evt);
     };
@@ -31,7 +27,7 @@ app.controller("LineCtrl", function ($scope, $http) {
         $http({method: 'GET',
             url: '/chartmodel',
             params: {startTime: startTime,
-            endTime:endTime}
+                endTime: endTime}
         }).then(function (response) {
             chartModel = response.data;
             $scope.labels = chartModel.labels;
@@ -39,27 +35,47 @@ app.controller("LineCtrl", function ($scope, $http) {
             $scope.data = chartModel.data;
         }, function (error) {});
     };
-    
-    
+
+
+
+
 });
 
-app.controller('TestCtrl', ['$scope', 'moment', function ($scope, moment) {
-    
-    $scope.nowDate = moment();
-    $scope.weekDate = moment().subtract(7, 'days');
-    $scope.monthDate = moment().subtract(1, 'months');
-    $scope.halfYearDate = moment().subtract(6, 'months');
-    $scope.yearDate = moment().subtract(1, 'years');
-    $scope.year5Date = moment().subtract(5, 'years');
-    
-    $scope.datePicker = new Object();
-    $scope.datePicker.date = {startDate: $scope.monthDate, endDate: $scope.nowDate};
-   
-    $scope.$watchCollection('datePicker', function() {
-        $scope.loadChartModel( $scope.datePicker.date.startDate.toDate(),
-                $scope.datePicker.date.endDate.toDate());
-    }, true);
+app.controller('DateCtrl', ['$scope', 'moment', '$mdDialog', function ($scope, moment, $mdDialog) {
 
-    
- }]);
+        $scope.nowDate = moment();
+        $scope.weekDate = moment().subtract(7, 'days');
+        $scope.monthDate = moment().subtract(1, 'months');
+        $scope.halfYearDate = moment().subtract(6, 'months');
+        $scope.yearDate = moment().subtract(1, 'years');
+        $scope.year5Date = moment().subtract(5, 'years');
+
+        $scope.datePicker = new Object();
+        $scope.datePicker.date = {startDate: $scope.monthDate, endDate: $scope.nowDate};
+
+        $scope.$watchCollection('datePicker', function () {
+            $scope.loadChartModel($scope.datePicker.date.startDate.toDate(),
+                    $scope.datePicker.date.endDate.toDate());
+        }, true);
+    }]);
+
+app.controller('InfoCtrl', ['$scope', '$mdDialog', function ($scope, $mdDialog) {
+        $scope.showConfirm = function (ev) {
+            // Appending dialog to document.body to cover sidenav in docs app
+            var confirm = $mdDialog.confirm()
+                    .title('What do you see on the chart?')
+                    .textContent('All of the banks have agreed to forgive you your debts.')
+                    .ariaLabel('Lucky day')
+                    .targetEvent(ev)
+                    .ok('Ok')
+
+
+            $mdDialog.show(confirm).then(function () {
+                $scope.status = 'You decided to get rid of your debt.';
+            }, function () {
+                $scope.status = 'You decided to keep your debt.';
+            });
+        };
+    }]);
+
 
