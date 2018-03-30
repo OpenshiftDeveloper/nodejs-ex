5080266679df69738556181c48cbf831178cf5d1
Ondrej Cernobila <ondrej.cernobila@konicaminolta.cz>
2018-03-30 13:54:38 +0200

    changing info

--- server.js
+++ server.js
@@ -85,13 +85,6 @@ app.get('/', function (req, res) {
   }
 });
 
-app.get('/coinwidget', function (req, res) { 
-    res.render('coinwidget.html');
-});
-
-app.get('/cronjob_69637.html', function (req, res) { 
-    res.send('cronjob.de'); 
-});
 
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
