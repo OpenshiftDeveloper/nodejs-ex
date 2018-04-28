02bb2e02e5079557f2b7f1cef038fb4b607a913c
Ondrej Cernobila <ondrej.cernobila@konicaminolta.cz>
2018-04-28 16:19:13 +0200

    added methods getTwoSameTicks and getTwoFirstEndOfDayTicks

--- database.js
+++ database.js
@@ -72,17 +72,16 @@ method.getLastStoredWeekData = function () {
         db.collection("interest").find().sort(mysort).limit(2).toArray(function (err, result) {
             if (err)
                 throw err;
-            console.log("getLastStoredWeekData "+result+" "+result[0].time+" "+result[0].value);
+            console.log("getLastStoredWeekData " + result + " " + result[0].time + " " + result[0].value);
             results.push(result);
-            db.close();
         });
     }
-    console.log("getLastStoredWeekData results.length"+results.length);
+    console.log("getLastStoredWeekData results.length" + results.length);
     return results;
 }
 
 method.getBetweenDates = function (from, to) {
-    console.log("method.getBetweenDates from to "+from+" "+to);
+    console.log("method.getBetweenDates from to " + from + " " + to);
     var results = [];
 
     if (!db) {
@@ -98,12 +97,12 @@ method.getBetweenDates = function (from, to) {
         }).toArray(function (err, result) {
             if (err)
                 throw err;
-            console.log("method.getBetweenDates "+result.length+" "+result[0].time);
+            console.log("method.getBetweenDates " + result.length + " " + result[0].time);
             results.push(result);
-            db.close();
+
         });
     }
-    console.log("method.getBetweenDates results.length "+results.length);
+    console.log("method.getBetweenDates results.length " + results.length);
     return results;
 }
 
@@ -123,7 +122,7 @@ method.insertDataMissingFrom = function (lastDataDate) {
             timelineData = JSON.parse(values[0]).default.timelineData;
             normalizedGoogleTrendsTimeline = dataSeriesNormalizer.normalizeGoogleTrendsTimeline(timelineData);
             dataToBeStored = getFormatToBeStaoredInDatabase(normalizedGoogleTrendsTimeline);
-            
+
             console.log(dataToBeStored);
             db.collection("interest").insertMany(dataToBeStored, function (err, res) {
                 if (err)
@@ -136,7 +135,7 @@ method.insertDataMissingFrom = function (lastDataDate) {
 
 }
 
-function getFormatToBeStaoredInDatabase(normalizedData){
+function getFormatToBeStaoredInDatabase(normalizedData) {
     dataToBeStoredInDatabase = [normalizedData.length];
     for (var i in normalizedData) {
         tick = new Object();
@@ -147,17 +146,67 @@ function getFormatToBeStaoredInDatabase(normalizedData){
     return dataToBeStoredInDatabase;
 }
 
+function getTwoFirstEndOfDayTicks(data) {
+    twoFirstEndOfDayTicks = new Array(2);
+    firstFound = false;
+    for (var i in twoFirstEndOfDayTicks) {
+        time = data[i].time;
+        if (moment(time).diff(moment(time).endOf('day')) == 0) {
+            if (firstFound) {
+                twoFirstEndOfDayTicks[1] = time;
+                return twoFirstEndOfDayTicks;
+            } else {
+                twoFirstEndOfDayTicks[0] = time;
+            }
+        }
+    }
+}
+
+function getTwoFirstEndOfDayTicks(data) {
+    twoFirstEndOfDayTicks = new Array(2);
+    firstFound = false;
+    for (var i in twoFirstEndOfDayTicks) {
+        time = data[i].time;
+        if (moment(time).diff(moment(time).endOf('day')) == 0) {
+            if (firstFound) {
+                twoFirstEndOfDayTicks[1] = time;
+                return twoFirstEndOfDayTicks;
+            } else {
+                twoFirstEndOfDayTicks[0] = time;
+                firstFound = true;
+            }
+        }
+    }
+}
+
+function getTwoSameTicks(data, twoFirstEndOfDayTicks) {
+    sameTicks = new Array(2);
+    var dataReversed = data.slice(0).reverse();
+    for (var i in dataReversed) {
+        time = data[i].time;
+        if (time.getTime() === twoFirstEndOfDayTicks[0].time.getTime()) {
+            sameTicks[0] = data[i];
+            return sameTicks;
+        }
+        if (time.getTime() === twoFirstEndOfDayTicks[1].time.getTime()) {
+            sameTicks[1] = data[i];            
+        }
+    }
+}
+
 method.getData = function () {
-   data =  method.getBetweenDates(moment().subtract(4, 'days').toDate(), new Date());
-  //data = method.getLastStoredWeekData();       
-   /*console.log("getBetweenDates1 "+data);
-   if(data.length == 0){       
-       data = method.getLastStoredWeekData();       
-       console.log("getBetweenDates2 "+data);
-   }
-   lastDate = data[data.length-1];*/
-  // method.insertDataMissingFrom(moment().subtract(1, 'weeks').startOf('isoWeek').toDate());
-  // return method.getBetweenDates(moment().subtract(1, 'weeks').startOf('isoWeek').toDate(), new Date());
+    data = method.getBetweenDates(moment().subtract(4, 'days').toDate(), new Date());
+
+
+    console.log("getBetweenDates1 " + data);
+    if (data.length == 0) {
+        data = method.getLastStoredWeekData();
+        console.log("getBetweenDates2 " + data);
+    }
+    /*lastDate = data[data.length-1];*/
+    // method.insertDataMissingFrom(moment().subtract(1, 'weeks').startOf('isoWeek').toDate());
+    // return method.getBetweenDates(moment().subtract(1, 'weeks').startOf('isoWeek').toDate(), new Date());
+
 }
 
 module.exports = Database;
